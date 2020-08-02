ccb925b9053e7163d66210f2e3728452ed039192
Brad <brd@brd-xps9300>
2020-08-02 17:37:23 -0400

    added energize and de-energize all command as well as some renaming of functions

--- i2c_backend.c
+++ i2c_backend.c
@@ -2,7 +2,7 @@
 // Uses the Linux kernel to send and receive data from a Tic.
 // See https://www.kernel.org/doc/Documentation/i2c/dev-interface for
 // more information.
- 
+
 #include <fcntl.h>
 #include <unistd.h>
 #include <linux/i2c.h>
@@ -14,6 +14,10 @@
 #include <string.h>
 #include <sys/ioctl.h>
 
+// The following definitions assume the i2c addresses are in sequential order.
+// For example, the default addresses are 11, 12, 13, 14, 15, 16.
+#define addr_top 16
+#define addr_bottom 11
 
   //Structure of using io control in linux for reference.
 
@@ -29,7 +33,7 @@
 //
 
 // Opens the specified I2C device.
-int open_i2c_device(const char* device) {
+int open_device(const char* device) {
   int fd = open(device, O_RDWR); //Open as given in fcntl.h
   if (fd == -1) {
     perror(device);
@@ -52,7 +56,7 @@ bool exit_safe_start(int fd, uint8_t address) {
 }
  
 // Sets the target position
-bool target_position(int fd, uint8_t address, int32_t target) {
+bool set_position(int fd, uint8_t address, int32_t target) {
   uint8_t command[] = {
     0xE0,
     (uint8_t)(target >> 0  & 0xFF),		//Split 32 bit register into four bytes
@@ -99,6 +103,8 @@ bool de_energize(int fd, uint8_t address) {
 void print_cmds() {
   printf("--energize ADR\t\tEnergize the motor at address ADR\n");
   printf("--de-energize ADR\t\tDe-energize the motor at address ADR\n");
+  printf("--energize-all\t\tEnergizes all motors.");
+  printf("--de-energize-all\t\tDe-energizes all motors.");
   printf("--set-position ADR PSTN\t\tSet the motor at address ADR to position PSTN\n");
   printf("--help\t\tPrint out command information\n");
 }
@@ -111,7 +117,7 @@ int main(int argc, char* argv[argc+1]) {
   //I2C device of the raspberry pi (GPIO2 & GPIO3).
   const char* device = "/dev/i2c-1";
  
-  int fd = open_i2c_device(device);
+  int fd = open_device(device);
   if (fd < 0)
     return EXIT_FAILURE;
  
@@ -145,6 +151,36 @@ int main(int argc, char* argv[argc+1]) {
       return EXIT_FAILURE;
     printf("Motor de-energized.\n");
   }
+  else if (strcmp(cmd, "--energize-all") == 0){
+    for (int i = addr_top; i >= addr_bottom; i--)
+    {
+      result = exit_safe_start(fd, i);
+      if(result)
+        return EXIT_FAILURE;
+      printf("Energizing motor %d.\n", i);
+      result = energize(fd, i);
+      if(result)
+        return EXIT_FAILURE;
+      printf("Motor at address %d energized.\n", i);
+    }
+    printf("All motors energized.\n");
+      return EXIT_SUCCESS;
+  }
+  else if (strcmp(cmd, "--de-energize-all") == 0){
+    for (int i = addr_top; i >= addr_bottom; i--)
+    {
+      result = exit_safe_start(fd, i);
+      if(result)
+        return EXIT_FAILURE;
+      printf("De-energizing motor %d.\n", i);
+      result = de_energize(fd, i);
+      if(result)
+        return EXIT_FAILURE;
+      printf("Motor at address %d de-energized.\n", i);
+    }
+    printf("All motors de-energized.\n");
+      return EXIT_SUCCESS;
+  }
   else if (strcmp(cmd, "--set-position") == 0) {
       const uint8_t address = strtol(argv[2], NULL, 10);
       result = exit_safe_start(fd, address);
@@ -154,7 +190,7 @@ int main(int argc, char* argv[argc+1]) {
       // Get the desired position value 
       int32_t position = strtol(argv[3], NULL, 10);
       printf("Setting target position to %d.\n", position);
-      result = target_position(fd, address, position);
+      result = set_position(fd, address, position);
         if (result) 
           return EXIT_FAILURE;
       printf("Position set.\n");
