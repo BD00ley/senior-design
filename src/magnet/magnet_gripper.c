a6a398dbfe1a4a1cf986944762ac03b80a024146
Brad <brd@brd-xps9300>
2020-09-09 12:52:06 -0400

    initial commit for magnet gripper

---
+++ src/magnet/magnet_gripper.c
@@ -0,0 +1,38 @@
+// Author: Bradley Dooley, USF EE.
+// This code allows for control of the magnetic gripper using
+// the bcm2835 library. See http://www.airspayce.com/mikem/bcm2835/modules.html
+// For full documentation on the library.
+
+#include <stdio.h>
+#include <bcm2835.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#define MPIN RPI_BPLUS_GPIP_J8_15  //Pin 15 on J8 header = GPIO22 = GPIO_GEN3 on RPi
+int main(int argc, char* argv[argc+1]) {
+
+if(argc > 1) {
+    printf("Too many arguments.\n");
+    return EXIT_FAILURE;
+}
+
+const char* cmd = argv[1];
+if(!bcm2835_init()){
+    perror("failed initialize bcm2835 library");
+    return EXIT_FAILURE;
+}
+
+bcm2835_gpio_fsel(MPIN, BCM2835_GPIO_FSEL_OUTP);
+if(strcmp(cmd, "--on")) {
+    bcm2835_gpio_write(MPIN, HIGH);
+}
+else if (strcmp(cmd,"--off")) {
+     bcm2835_gpio_write(MPIN, LOW);
+}
+else
+{
+    printf("Unknown command. Use --on or -off for magnet gripper control.\n")
+}
+
+return EXIT_SUCCESS; 
+}
