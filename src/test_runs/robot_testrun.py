77939218f1b6fc90334b39e5a4584a81198d0db2
Brad <brd@brd-xps9300>
2020-09-17 13:22:03 -0400

    new movements

--- src/test_runs/robot_testrun.py
+++ src/test_runs/robot_testrun.py
@@ -7,31 +7,20 @@ elbow_addr = 14
 middlew_addr = 12
 outerw_addr = 11
 
-call(["./i2c_backend", "--set-position", str(base_addr), str(50)])
+#call(["./magnet_gripper", "--on"})
+call(["./i2c_backend", "--set-position", str(base_addr), str(-40)])
+time.sleep(0.5)
+call(["./i2c_backend", "--set-position", str(base_addr), str(0)])
 time.sleep(0.5)
 
-call(["./i2c_backend", "--set-position", str(elbow_addr), str(300)])
-time.sleep(1.5)
+call(["./i2c_backend", "--set-position", str(elbow_addr), str(-50)])
+time.sleep(0.5)
 
-call(["./i2c_backend", "--set-position", str(middlew_addr), str(50)])
-time.sleep(1.2)
-
-call(["./i2c_backend", "--set-position", str(outerw_addr), str(10)])
-time.sleep(0.4)
-
-call(["./i2c_backend", "--set-position", str(outerw_addr), str(-10)])
-time.sleep(0.4)
-
-call(["./i2c_backend", "--set-position", str(outerw_addr), str(10)])
-time.sleep(0.4)
-
-call(["./i2c_backend", "--set-position", str(outerw_addr), str(-10)])
-time.sleep(0.4)
-
-call(["./i2c_backend", "--set-position", str(outerw_addr), str(0)])
-time.sleep(1.2)
-
+call(["./i2c_backend", "--set-position", str(middlew_addr), str(-60)])
+time.sleep(5.0)
 
 call(["./i2c_backend", "--set-position", str(middlew_addr), str(0)])
-call(["./i2c_backend", "--set-position", str(elbow_addr), str(300)])
-call(["./i2c_backend", "--set-position", str(base_addr), str(50)])
+call(["./i2c_backend", "--set-position", str(elbow_addr), str(0)])
+call(["./i2c_backend", "--set-position", str(base_addr), str(-10)])
+time.sleep(2.0)
+#call(["./magnet_gripper", "--off"])
