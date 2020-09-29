4304b2658132818ed891d3e3d09585aa00e13a6a
Brad <brd@brd-xps9300>
2020-09-29 18:15:29 -0400

    small bugfix on initial commit

--- src/matlab/JointAngle.m
+++ src/matlab/JointAngle.m
@@ -1,12 +1,12 @@
 function JointAngle(RPI, base_angle, shoulder_angle, elbow_angle, inner_wrist_angle, middle_wrist_angle, outer_wrist_angle)
 % JointAngle: A basic function that allows for control of each arm joint.
 % Note: Raspberry pi must be connected.
-system(RPI, char("cd ~/Desktop/senior-design/test_runs; ./i2c_backend " + "--set-position " + "16" + base_angle));
-system(RPI, char("cd ~/Desktop/senior-design/test_runs; ./i2c_backend " + "--set-position " + "15" + shoulder_angle));
-system(RPI, char("cd ~/Desktop/senior-design/test_runs; ./i2c_backend " + "--set-position " + "14" + elbow_angle));
-system(RPI, char("cd ~/Desktop/senior-design/test_runs; ./i2c_backend " + "--set-position " + "13" + inner_wrist_angle));
-system(RPI, char("cd ~/Desktop/senior-design/test_runs; ./i2c_backend " + "--set-position " + "12" + middle_wrist_angle));
-system(RPI, char("cd ~/Desktop/senior-design/test_runs; ./i2c_backend " + "--set-position " + "11" + outer_wrist_angle));
+system(RPI, char("cd ~/Desktop/senior-design/test_runs; ./i2c_backend " + "--set-position " + "16 " + base_angle));
+system(RPI, char("cd ~/Desktop/senior-design/test_runs; ./i2c_backend " + "--set-position " + "15 " + shoulder_angle));
+system(RPI, char("cd ~/Desktop/senior-design/test_runs; ./i2c_backend " + "--set-position " + "14 " + elbow_angle));
+system(RPI, char("cd ~/Desktop/senior-design/test_runs; ./i2c_backend " + "--set-position " + "13 " + inner_wrist_angle));
+system(RPI, char("cd ~/Desktop/senior-design/test_runs; ./i2c_backend " + "--set-position " + "12 " + middle_wrist_angle));
+system(RPI, char("cd ~/Desktop/senior-design/test_runs; ./i2c_backend " + "--set-position " + "11 " + outer_wrist_angle));
 
 end
 
